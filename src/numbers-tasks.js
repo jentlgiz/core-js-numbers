9d3a59f732fd305a7d9ff7e5ec2b40fe6da4c970
Nargiz Sabitova <sabitova.nargiz@yandex.ru>
2024-12-09 23:24:28 +0500

    feat: 9th task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -166,8 +166,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**
@@ -187,8 +187,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const number = num / 10 ** pow;
+  const roundedNumber = Math.round(number);
+  const result = roundedNumber * 10 ** pow;
+  return result;
 }
 
 /**
@@ -208,8 +211,16 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
